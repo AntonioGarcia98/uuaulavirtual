48e2d7eea8c1165491bde69446a2e5594a4a47ad
Antonio Garcia <xccabx@gmail.com>
2020-06-13 16:51:59 -0500

    commit agregar campos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -72,7 +72,7 @@ export class AppComponent {
       email: new FormControl(null, Validators.email),
       /* phone_number : new FormControl(null, []), */
       password: new FormControl(null),
-      userType: new FormControl(null),
+      userType: new FormControl(1),
       scholarship: new FormControl(null),
       grade: new FormControl(null),
 
