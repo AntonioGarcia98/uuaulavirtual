4e58da1a5b061a722fbfa9f56c795edee11601e3
Saul Fuantos <luisust10@gmail.com>
2020-06-13 21:09:44 -0500

    Login update

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -3,6 +3,7 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { ServiceBase } from './base.service';
 import { User, Student } from '../models/user.model';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class StudentService extends ServiceBase<Student> {
