ddc3ad6ac974ae01114fce2d3868ae0ad2763bde
beris147 <edwir147@gmail.com>
2020-06-16 19:36:10 -0500

    get rooms by group and by user id

--- server/server/controllers/room.js
+++ server/server/controllers/room.js
@@ -1,8 +1,9 @@
 const {verify, admin, userid, teacher} = require('../middlewares/auth')
-const {create, find} = require('../config/functions')
+const {create, find, ok, errorHandler, notFound} = require('../config/functions')
 
 const Room = require('../models/room')
 const { select } = require('underscore')
+const { group } = require('console')
 
 let room = { 
     name: '/room', 
@@ -37,12 +38,34 @@ let room = {
     },
     extra: app => {
         app.get('/rooms/group/:id', (req, res) => find(Room, { group: req.params.id }, req, res)),
-        app.get('/groups/user/:id', (req, res) => find(Room, 
-            {$or: [
-                {teachers: {$in : [req.params.id] }}, 
-                {students: {$in : [req.params.id] }}
-            ]}, req, res, 
-            { "group": 1, "_id": 0, "students": 0, "teachers": 0}))
+        // app.get('/rooms/group/:group/user/:user'), (req, res) => find(Room, {$and: [ { group: req.params.group },{$or: [{teachers: {$in : [req.params.user] }}, {students: {$in : [req.params.user] } }] } ]}, req, res),
+        app.get('/groups/user/:id', (req, res) => {
+            let page = Number(req.query.page || 1)
+            let limit = Number(req.query.limit || 5)
+            Room.find( {$or: [{teachers: {$in : [req.params.id] }}, {students: {$in : [req.params.id] } }] } )
+                .skip((page-1) * limit)
+                .limit(limit)
+                .distinct('group')
+                .exec((err, items) => {
+                    if(err) return errorHandler(err, res)
+                    if(!items || !items.length) return notFound(res)
+                    return ok(items, res)
+                })
+        }),
+        app.get('/rooms/group/:group/user/:user', (req, res) => find(Room, 
+            {
+                $and: 
+                [ 
+                    { group: req.params.group },
+                    {
+                        $or: 
+                        [
+                            { teachers: {$in: [req.params.user]} }, 
+                            { students: {$in: [req.params.user]} }
+                        ] 
+                    } 
+                ]
+            }, req, res))
     }
 }
 
